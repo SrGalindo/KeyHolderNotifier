d8ac72f3a388072ee96912dad80eedb40747ebc3
Galindo <magalindo8@gmail.com>
2021-06-16 16:54:45 +0200

    merge simple compila  y funciona

--- src/main.ino
+++ src/main.ino
@@ -32,6 +32,48 @@
  #define DEBUG_PRINTF(x)
 #endif
 
+//Start plug section
+const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+
+int sensorValue = 0;  // value read from the pot
+
+const float INICIO_1M                = 255;
+const float FIN_1M                   = 264;
+const float INICIO_180K              = 341;
+const float FIN_180K                 = 355;
+const float INICIO_470K              = 432;
+const float FIN_470K                 = 438;
+const float INICIO_1M_180K           = 469;
+const float FIN_1M_180K              = 480;
+const float INICIO_1M_470K           = 532;
+const float FIN_1M_470K              = 537;
+const float INICIO_300K              = 543;
+const float FIN_300K                 = 554;
+const float INICIO_470K_180K         = 567;
+const float FIN_470K_180K            = 580;
+const float INICIO_1M_300K           = 612;
+const float FIN_1M_300K              = 621;
+const float INICIO_1M_470K_180K      = 629;
+const float FIN_1M_470K_180K         = 639;
+const float INICIO_300K_180K         = 640;
+const float FIN_300K_180K            = 652;
+const float INICIO_300K_470K         = 674;
+const float FIN_300K_470K            = 679;
+const float INICIO_1M_300K_180K      = 687;
+const float FIN_1M_300K_180K         = 700;
+const float INICIO_1M_300K_470K      = 712;
+const float FIN_1M_300K_470K         = 722;
+const float INICIO_300K_470K_180K    = 730;
+const float FIN_300K_470K_180K       = 740;
+const float INICIO_1M_300K_470K_180K = 756;
+const float FIN_1M_300K_470K_180K    = 770;
+
+long previousMillis = 0;
+const long intervalRead = 2000;
+boolean previousPlayedStatusPlug[4];
+boolean justReadStatusPlug[4];
+//End plug section
+
 WiFiManager wifiManager;
 WiFiClient                wifiClient;
 PubSubClient              mqttClient(wifiClient);
@@ -106,6 +148,12 @@ void setup() {
   Serial.begin(115200);
   pinMode(relePin, OUTPUT);
   digitalWrite(relePin, HIGH);
+
+  for(int i=0; i<4; i++){
+    previousPlayedStatusPlug[i]=0;
+    justReadStatusPlug[i]=0;
+  }
+
   wifiSetup();
 
   DEBUG_PRINT(F("server --> "));
@@ -127,7 +175,9 @@ void setup() {
   out = new AudioOutputI2SNoDAC();
   DEBUG_PRINTLN("Using No DAC - using Serial port Rx pin");
   out->SetGain(volume_level);
+  DEBUG_PRINTLN("Before playing boot sound");
   playBootSound();
+  DEBUG_PRINTLN("After playing boot sound");
 }
  
 void loop() {
@@ -136,6 +186,12 @@ void loop() {
   if (mp3   && !mp3->loop())    stopPlaying();
   if (wav   && !wav->loop())    stopPlaying();
   if (rtttl && !rtttl->loop())  stopPlaying();
+  long currentMillis = millis();
+  if (currentMillis - previousMillis >= intervalRead) {
+    previousMillis = currentMillis;
+    checkPlug();
+    compareNewPlug();
+  }
 
 }
 
@@ -448,4 +504,121 @@ void actionPrePlay(){
   digitalWrite(relePin, LOW);
   delay(300);
   broadcastStatus("status", "playing");
+}
+
+//plugs methods
+void compareNewPlug(){
+  for(int i=0; i<4; i++){
+    if(justReadStatusPlug[i] != previousPlayedStatusPlug[i]){
+      if(justReadStatusPlug[i] == 1){
+        DEBUG_PRINT("Playing sound");
+        DEBUG_PRINTLN(i);
+      }else{
+        DEBUG_PRINT("Playing sound 5");
+      }
+    }
+    previousPlayedStatusPlug[i] = justReadStatusPlug[i];
+  }
+}
+
+void checkPlug(){
+  sensorValue = analogRead(analogInPin);
+  DEBUG_PRINT(" sensorValue=");
+  DEBUG_PRINTLN(sensorValue);
+  if(sensorValue < INICIO_1M){
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_1M < sensorValue && sensorValue < FIN_1M){
+    DEBUG_PRINTLN("1M");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_180K < sensorValue && sensorValue < FIN_180K){
+    DEBUG_PRINTLN("180K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_470K < sensorValue && sensorValue < FIN_470K){
+    DEBUG_PRINTLN("470K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_1M_180K < sensorValue && sensorValue < FIN_1M_180K){
+    DEBUG_PRINTLN("1M 180K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_1M_470K < sensorValue && sensorValue < FIN_1M_470K){
+    DEBUG_PRINTLN("1M 470K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_300K < sensorValue && sensorValue < FIN_300K){
+    DEBUG_PRINTLN("300K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_470K_180K < sensorValue && sensorValue < FIN_470K_180K){
+    DEBUG_PRINTLN("470K 180K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_1M_300K < sensorValue && sensorValue < FIN_1M_300K){
+    DEBUG_PRINTLN("1M 300K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_1M_470K_180K < sensorValue && sensorValue < FIN_1M_470K_180K ){
+    DEBUG_PRINTLN("1M 470K 180K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=0;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_300K_180K < sensorValue && sensorValue < FIN_300K_180K){
+    DEBUG_PRINTLN("300K 180K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_300K_470K < sensorValue && sensorValue < FIN_300K_470K){
+    DEBUG_PRINTLN("300K 470K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_1M_300K_180K < sensorValue && sensorValue < FIN_1M_300K_180K){
+    DEBUG_PRINTLN("1M 300K 180K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=0;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_1M_300K_470K < sensorValue && sensorValue < FIN_1M_300K_470K){
+    DEBUG_PRINTLN("1M 300K 470K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=0;
+  }else if(INICIO_300K_470K_180K < sensorValue && sensorValue < FIN_300K_470K_180K){
+    DEBUG_PRINTLN("300K 470K 180K");
+    justReadStatusPlug[0]=0;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=1;
+  }else if(INICIO_1M_300K_470K_180K < sensorValue && sensorValue < FIN_1M_300K_470K_180K){
+    DEBUG_PRINTLN("1M 300K 470K 180K");
+    justReadStatusPlug[0]=1;
+    justReadStatusPlug[1]=1;
+    justReadStatusPlug[2]=1;
+    justReadStatusPlug[3]=1;
+  }
 }
